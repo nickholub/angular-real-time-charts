98868bbb09224e667a268940c831777ae8cc25e6
Nick Holub <nikholub@gmail.com>
2013-10-27 14:22:25 -0700

    Yeoman AngularJS starter app

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('angularRealTimeChartsApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
