af49420836f36ab2adf8d57516f8312824ce1b7e
nikholub@gmail.com <nikholub@gmail.com>
2014-07-14 16:52:36 -0700

    Upgrade to AngularJS 1.2.19

--- karma.conf.js
+++ karma.conf.js
@@ -13,6 +13,7 @@ module.exports = function(config) {
     files: [
       'app/bower_components/jquery/jquery.js',
       'app/bower_components/angular/angular.js',
+      'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/bower_components/angular-resource/angular-resource.js',
       'app/bower_components/angular-cookies/angular-cookies.js',
